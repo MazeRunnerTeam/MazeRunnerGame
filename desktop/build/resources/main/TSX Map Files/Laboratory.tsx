f93509d9eb5b4b670fb3d9afcbf22fec9c36ba2a
Sergey Sarkisyan <sergey.s.sarkisyan@gmail.com>
2025-05-29 21:49:00 +0200

    reorganizing
    removed unnecessary root folder

---
+++ desktop/build/resources/main/TSX Map Files/Laboratory.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Laboratory" tilewidth="32" tileheight="32" tilecount="192" columns="12">
+ <image source="../Sprites/tiles_laboratory_LandOfPixels/32px/tilesWalls.png" width="384" height="512"/>
+</tileset>
---
+++ desktop/build/resources/main/TSX Map Files/Laboratory2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Laboratory2" tilewidth="32" tileheight="32" tilecount="592" columns="37">
+ <image source="../Sprites/tiles_laboratory_LandOfPixels/32px/tilesStuff.png" width="1184" height="512"/>
+</tileset>
